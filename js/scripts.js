c1d554a3ec4692e34bcb90a4b104f2bc777a0e88
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-15 11:24:05 -0700

    set up initial file structure

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var replacer = function(sentence, word, replacement) {
+  return sentence;
+};
+
+
+// $(document).ready(function(){
+
+//   $("form#replacer").submit(function(event){
+//     event.preventDefault();
+//     var sentence = $("input#sentence").val());
+//     var word = $("input#word").val());
+//     var replacement = $("input#replacement").val());
+//     var result = replacer(sentence, word, replacement);
+//     $(".final").text(" ");
+    
+//     if(!result) {
+//       $(".final").text("Uggh there was some sort of problem encountered. Try again");
+//     } 
+
+//     $("#result").show();
+
+//   });//end of submit event
+// });//end of file
---
+++ spec/specs.js
@@ -0,0 +1,9 @@
+describe('replacer', function() {
+  it("returns a string", function() {
+    expect(replacer("bob is great", "bob", "Sam")).to.equal("bob is great");
+  });
+
+  
+
+});
+
