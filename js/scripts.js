4786e7d01e9c5d82617a66c7db2ec5eb53160527
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-15 12:39:43 -0700

    adds animation and basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,27 @@ var replacer = function(sentence, word, replacement) {
 };
 
 
-// $(document).ready(function(){
-
-//   $("form#replacer").submit(function(event){
-//     event.preventDefault();
-//     var sentence = $("input#sentence").val());
-//     var word = $("input#word").val());
-//     var replacement = $("input#replacement").val());
-//     var result = replacer(sentence, word, replacement);
-//     $(".final").text(" ");
+$(document).ready(function(){
+ var endEvent = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+  $("form#replacer").submit(function(event){
+    event.preventDefault();
+    var sentence = $("input#sentence").val();
+    var word = $("input#word").val();
+    var replacement = $("input#replacement").val();
+    var result = replacer(sentence, word, replacement);
+    $(".final").text(" ");
     
-//     if(!result) {
-//       $(".final").text("Uggh there was some sort of problem encountered. Try again");
-//     } 
+    if(!result) {
+      $(".final").text("Uggh there was some sort of problem encountered. Try again");
+    } else {
+      $(".final").text(result);
+    }
 
-//     $("#result").show();
+    $("#result").show().addClass("animated bounceInLeft").one(endEvent,
+      function(){
+        $(this).removeClass("animated bounceInLeft");
+    });
 
-//   });//end of submit event
-// });//end of file
+
+  });//end of submit event
+});//end of file
