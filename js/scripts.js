26e243b732c4b683ecc7a1507300417d3322e278
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-15 12:03:41 -0700

    adds regex function as well as unit tests for function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 var replacer = function(sentence, word, replacement) {
-  return sentence;
+  //check for user error
+  if((word.length === 0) || (replacement.length === 0)|| (sentence.length === 0) ) {
+      return false;
+  }
+  //create RegExp object so that it can be passed into replace method
+  var regex = new RegExp(word, "gi");
+  var fixedSentence = sentence.replace(regex, replacement);
+
+  return fixedSentence;
+
 };
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,31 @@
 describe('replacer', function() {
   it("returns a string", function() {
-    expect(replacer("bob is great", "bob", "Sam")).to.equal("bob is great");
+    expect(replacer("bob is great", "bob", "Sam")).to.be.a("string");
   });
 
-  
+  it("replaces one word ", function() {
+    expect(replacer("bob", "bob", "Sam")).to.equal("Sam");
+  });
+
+  it("matches words regardless of case", function() {
+    expect(replacer("bob is great", "Bob", "Sam")).to.equal("Sam is great");
+  });
+
+  it("matches words occuring anywhere regardless of spacing", function() {
+    expect(replacer("bob is bobgreat", "Bob", "Sam")).to.equal("Sam is Samgreat");
+  });
+
+  it("returns false if user doesn't enter a word to find", function() {
+    expect(replacer("Sam is Samgreat", "", "bob")).to.equal(false);
+  });
+
+  it("returns false if user doesn't enter a sentence to find", function() {
+    expect(replacer("", "sam", "bob")).to.equal(false);
+  });
+
+  it("returns false if user doesn't enter a replacement to find", function() {
+    expect(replacer("blah sam", "sam", "")).to.equal(false);
+  });
 
 });
 
