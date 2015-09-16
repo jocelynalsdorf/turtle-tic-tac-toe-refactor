e0003ce0ade55f7362bf915646be3474a2f7c4c1
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-16 10:27:29 -0700

    sets up base forms in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-function Contact(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.addresses = [];
+function Drink(base, size) {
+  this.base = base;
+  this.size = size;
+  this.mixins = [];
 }
 
-Contact.prototype.fullName = function(){
-  return this.firstName + " " + this.lastName;
+Drink.prototype.price = function(){
+  
 }
 
-function Address(street, city, state) {
-  this.street = street;
-  this.city = city;
-  this.state = state;
+function Mixin(itemOne, itemTwo, itemThree) {
+  this.itemOne = itemOne;
+  this.itemTwo = itemTwo;
+  this.itemThree = itemThree;
 }
 
-Address.prototype.fullAddress = function(){
-  return this.street + ", " + this.city + ", " + this.state;
+Mixin.prototype.addons = function(){
+  
 }
 
 
@@ -28,64 +28,48 @@ var resetFields = function(){
   $("input.new-state").val("");
   };
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' + 
-                     '<div class="form-group">' + 
-                       '<label for="new-street">Street</label>' + 
-                       '<input type="text" class="form-control new-street">' + 
-                     '</div>' + 
-                     '<div class="form-group">' + 
-                       '<label for="new-city">City</label>' + 
-                       '<input type="text" class="form-control new-city">' + 
-                     '</div>' + 
-                     '<div class="form-group">' + 
-                       '<label for="new-state">State</label>' + 
-                       '<input type="text" class="form-control new-state">' + 
-                     '</div>' + 
-                   '</div>');
-  });
-
+  
 
 
 
  
-  $("form#new-contact").submit(function(event){
-    event.preventDefault();
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+//   $("form#new-contact").submit(function(event){
+//     event.preventDefault();
+//     var inputtedFirstName = $("input#new-first-name").val();
+//     var inputtedLastName = $("input#new-last-name").val();
     
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+//     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $(".new-address").each(function() {
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
+//     $(".new-address").each(function() {
+//       var inputtedStreet = $(this).find("input.new-street").val();
+//       var inputtedCity = $(this).find("input.new-city").val();
+//       var inputtedState = $(this).find("input.new-state").val();
 
-      var newAddress = new Address(inputtedStreet,inputtedCity, inputtedState);
-      newContact.addresses.push(newAddress);
-     });
+//       var newAddress = new Address(inputtedStreet,inputtedCity, inputtedState);
+//       newContact.addresses.push(newAddress);
+//      });
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+//     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
 
 
-    $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
+//     $(".contact").last().click(function() {
+//     $("#show-contact").show();
+//     $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+//     $(".first-name").text(newContact.firstName);
+//     $(".last-name").text(newContact.lastName);
 
-    $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address){
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
+//     $("ul#addresses").text("");
+//       newContact.addresses.forEach(function(address){
+//         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+//       });
 
-    $("ul#addresses").text(newContact.address);
-    });
+//     $("ul#addresses").text(newContact.address);
+//     });
 
-    resetFields();
+//     resetFields();
 
-  });//end of submit event
-});//end of file
+//   });//end of submit event
+// });//end of file
