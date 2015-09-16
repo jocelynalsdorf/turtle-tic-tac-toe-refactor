6268ef8d34cb80672585403a55555a07a78def9c
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-16 14:14:18 -0700

    indentation fixes

--- js/scripts.js
+++ js/scripts.js
@@ -55,14 +55,12 @@ Drink.prototype.price = function(quantity){
     }
 
 
-
 var resetFields = function(){
   $("input#how-many").val("");
   $("input#drink-name").val("");
   };
 
 $(document).ready(function(){
-
 
 $("form#new-drink").submit(function(event){
   event.preventDefault();
@@ -75,15 +73,9 @@ $("form#new-drink").submit(function(event){
   var fruitType = $('input[name="fruit-type"]:checked').val(); 
   var syrupType = $('input[name="syrup-type"]:checked').val(); 
 
+  var newDrink = new Drink(baseType, drinkSize, drinkName, boozeType, fruitType, syrupType);
 
-
-     var newDrink = new Drink(baseType, drinkSize, drinkName, boozeType, fruitType, syrupType);
-  console.log(newDrink);
-
-
-  console.log(newDrink.price(howManyDrinks));
   $(".cost-span").text(newDrink.price(howManyDrinks));
-
   $(".base-info").text(baseType);
   $(".size-info").text(drinkSize);
   $(".name-info").text(drinkName);
@@ -92,36 +84,7 @@ $("form#new-drink").submit(function(event){
   $(".fruit-info").text(fruitType);
   $(".quantity-info").text(howManyDrinks);
 
-
-
-//     $(".new-address").each(function() {
-//       var inputtedStreet = $(this).find("input.new-street").val();
-//       var inputtedCity = $(this).find("input.new-city").val();
-//       var inputtedState = $(this).find("input.new-state").val();
-
-//       var newAddress = new Address(inputtedStreet,inputtedCity, inputtedState);
-//       newContact.addresses.push(newAddress);
-//      });
-
-//     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-
-
-//     $(".contact").last().click(function() {
-//     $("#show-contact").show();
-//     $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-//     $(".first-name").text(newContact.firstName);
-//     $(".last-name").text(newContact.lastName);
-
-//     $("ul#addresses").text("");
-//       newContact.addresses.forEach(function(address){
-//         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-//       });
-
-//     $("ul#addresses").text(newContact.address);
-//     });
-
-    // resetFields();
+  resetFields();
 
   });//end of submit event
 });//end of file
