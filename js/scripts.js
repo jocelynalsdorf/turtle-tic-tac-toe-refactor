2ae9d8504c44f9d3655d6c2bb016dd3b3f2c2034
jocelynalsdorf <jfrodsla@gmail.com>
2015-09-16 14:41:43 -0700

    initial file setup

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,37 @@
-function Drink(base, size, name, booze, fruit, syrup) {
-  this.base = base;
-  this.size = size;
-  this.name = name;
-  this.booze = booze;
-  this.fruit = fruit;
-  this.syrup = syrup;
+function Game() {
+  this.;
+  this.;
+  this.;
+  this.;
+  this.;
+  this.;
 }
 
-Drink.prototype.price = function(quantity){
-  var totalPrice = 0;
-  if (this.size === "tall") {
-    totalPrice = 3;
-  } else {
-    totalPrice = 1;
-  }
 
-    if(this.base === "tomato-juice" || this.base === "fruit-juice" ){
-      totalPrice += 4;
-      if(this.fruit === "olive" || this.fruit === "orange" ){
-        totalPrice += 2;
-      } else {
-        totalPrice +=1;
-      }
-      if(this.syrup === "triple-sec" || this.syrup === "spicy") {
-        totalPrice += 2;
-      } else {
-        totalPrice += 1;
-      }
-      if(this.booze === "tequila" || this.booze === "gin") {
-        totalPrice += 2;
-      } else {
-        totalPrice += 1;
-      }
-    } else {
-      totalPrice += 3
-      if(this.fruit === "olive" || this.fruit === "orange" ){
-          totalPrice += 2;
-        } else {
-          totalPrice +=1;
-        }
-        if(this.syrup === "triple-sec" || this.syrup === "spicy") {
-          totalPrice += 2;
-        } else {
-          totalPrice += 1;
-        }
-        if(this.booze === "tequila" || this.booze === "gin") {
-          totalPrice += 2;
-        } else {
-          totalPrice += 1;
-        }
-    }
 
-      return totalPrice * quantity;
-    }
 
 
 var resetFields = function(){
-  $("input#how-many").val("");
-  $("input#drink-name").val("");
+  $("input#player1name").val("");
+  $("input#player2name").val("");
   };
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-$("form#new-drink").submit(function(event){
-  event.preventDefault();
+// $("form#new-game").submit(function(event){
+//   event.preventDefault();
 
-  var baseType = $('input[name="base-type"]:checked').val();
-  var drinkSize = $('input[name="drink-size"]:checked').val(); 
-  var howManyDrinks = parseInt($("input#how-many").val());
-  var drinkName = $("input#drink-name").val();
-  var boozeType = $('input[name="booze-type"]:checked').val();   
-  var fruitType = $('input[name="fruit-type"]:checked').val(); 
-  var syrupType = $('input[name="syrup-type"]:checked').val(); 
+//   var player1name = $("input#player1name").val();
+//   var player2name = $("input#player2name").val(); 
+  
 
-  var newDrink = new Drink(baseType, drinkSize, drinkName, boozeType, fruitType, syrupType);
+  
 
-  $(".cost-span").text(newDrink.price(howManyDrinks));
-  $(".base-info").text(baseType);
-  $(".size-info").text(drinkSize);
-  $(".name-info").text(drinkName);
-  $(".booze-info").text(boozeType);
-  $(".syrup-info").text(syrupType);
-  $(".fruit-info").text(fruitType);
-  $(".quantity-info").text(howManyDrinks);
+//   $(".player-one-score").text(blankfornow));
+//   $(".player-two-score").text(blankfornow));
+  
 
-  resetFields();
+//   //resetFields();
 
-  });//end of submit event
-});//end of file
+//   });//end of submit event
+// });//end of file
--- spec/specs.js
+++ spec/specs.js
@@ -1,54 +1,12 @@
-describe('Drink', function() {
-  it("creates a new drink with the given specifications", function(){
-    var testDrink = new Drink("coke", "tall","liz", "rum", "lime", "coconut");
-    expect(testDrink.base).to.equal("coke");
-    expect(testDrink.size).to.equal("tall");
-    expect(testDrink.name).to.equal("liz");
-    expect(testDrink.booze).to.equal("rum");
-    expect(testDrink.fruit).to.equal("lime");
-    expect(testDrink.syrup).to.equal("coconut");
+describe('Game', function() {
+  it("creates a new game with the given specifications", function(){
+    var testGame= new Game();
+    expect(testGame).to.equal();
+    expect(testGame).to.equal();
+    expect(testGame).to.equal();
+    expect(testGame).to.equal();
   });
 
-  it("returns correct price for tall drink with the tomato or juice base", function(){
-    var testDrink = new Drink("tomato-juice", "tall","todd", "rum", "lime", "grenidine");
-    expect(testDrink.base).to.equal("tomato-juice");
-    expect(testDrink.size).to.equal("tall");
-    expect(testDrink.name).to.equal("todd");
-    expect(testDrink.booze).to.equal("rum");
-    expect(testDrink.fruit).to.equal("lime");
-    expect(testDrink.syrup).to.equal("grenidine");
-    expect(testDrink.price(1)).to.equal(10);
-  });
-
-  it("returns correct price for tall drink with the club soda or coke base", function(){
-    var testDrink = new Drink("coke", "tall","jake", "gin", "olive", "coconut");
-    expect(testDrink.base).to.equal("coke");
-    expect(testDrink.size).to.equal("tall");
-    expect(testDrink.name).to.equal("jake");
-    expect(testDrink.booze).to.equal("gin");
-    expect(testDrink.fruit).to.equal("olive");
-    expect(testDrink.syrup).to.equal("coconut");
-    expect(testDrink.price(1)).to.equal(11);
-  });
-
-  it("returns correct price for short drink with the club soda or coke base", function(){
-    var testDrink = new Drink("coke", "short","jerry", "gin", "orange", "coconut");
-    expect(testDrink.price(1)).to.equal(9);
-  });
-
-  it("returns correct price for short drink with the tomato or juice base", function(){
-    var testDrink = new Drink("tomato-juice", "short","tim", "rum", "lime", "coconut");
-    expect(testDrink.price(1)).to.equal(8);
-  });
-
-  it("returns correct price for multiple short drinks with the tomato or juice base", function(){
-    var testDrink = new Drink("tomato-juice", "short","bob", "vodka", "lime", "spicy");
-    expect(testDrink.price(3)).to.equal(27);
-  });
-
-  it("returns correct price for multiple tall drinks with the coke or club soda", function(){
-    var testDrink = new Drink("club-soda", "tall","bob", "tequila", "cherry", "triple-sec");
-    expect(testDrink.price(15)).to.equal(165);
-  });
+  
 
 });
